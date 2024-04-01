96ce34768da3adfc58b3bfe5af534b25feb0a069
Lakhani Nawaz <lakhaninawaz1@gmail.com>
2024-04-01 14:59:45 +0530

    #

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    // depending on your application, base can also be "/"
-    base: '',
-    plugins: [react(), viteTsconfigPaths()],
-    server: {    
-        // this ensures that the browser opens upon server start
-        open: true,
-        // this sets a default port to 3000  
-        port: 3000, 
+  plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/))  return null
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        })
+      },
     },
+    react(),
+  ],
+
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
