330e1986620f2945143fbe1de5ab0119dc90c45c
Lakhani Nawaz <lakhaninawaz1@gmail.com>
2024-04-02 22:24:23 +0530

    #

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,20 @@
-import { defineConfig, transformWithEsbuild } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, transformWithEsbuild } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   
-      base: './',
+  base: './',
   plugins: [
     {
       name: 'treat-js-files-as-jsx',
       async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/))  return null
+        if (!id.match(/src\/.*\.js$/)) return null;
 
         // Use the exposed transform from vite, instead of directly
         // transforming with esbuild
         return transformWithEsbuild(code, id, {
           loader: 'jsx',
           jsx: 'automatic',
-        })
+        });
       },
     },
     react(),
@@ -29,4 +28,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
