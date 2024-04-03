d4cec2e056fb1d789f1d01f38ffc33414312b178
Lakhani Nawaz <lakhaninawaz1@gmail.com>
2024-04-03 12:59:47 +0530

    #

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:  import.meta.env.REACT_APP_BASE_URL
+    baseURL:  "https://pintrest-clone-mern-api.vercel.app"
 })
 
 export default instance;
