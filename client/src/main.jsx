4a9935d364737f18fd54bff0896994279355b91e
Lakhani Nawaz <lakhaninawaz1@gmail.com>
2024-04-13 17:14:35 +0530

    #

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './store/auth.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
         <React.StrictMode>
             <BrowserRouter>
                 <App />
+                <Analytics/>
             </BrowserRouter>
         </React.StrictMode>
     </AuthProvider>
